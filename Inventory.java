4ca5574125f3f5b795df10e9aaf3d40ce404dfc9
slck16 <84081500+slck16@users.noreply.github.com>
2022-04-05 23:31:45 -0600

    Update Inventory.java

--- Inventory.java
+++ Inventory.java
@@ -4,14 +4,22 @@ public class Inventory {
   private LinkedList<Food> inventoryList = new LinkedList<Food>();
   
   public Inventory (String url, String user, String password){
-  
+    // code to read from database
+    // a loop?
+    inventoryList.add(foodObj);
   }
 
   public remove (String itemID){
-  
+    for (int i = 0; i < inventoryList.size(); i++){
+      if (inventoryList.get(i).getID == itemID)
+        inventoryList.remove(i);
+    }
   }
   
+  // ********** Not sure if return is food object or string **********
   public String getItem (String itemID){
-  
-  }
+    for (int i = 0; i < inventoryList.size(); i++){
+      if (inventoryList.get(i).getID == itemID)
+        return inventoryList.get(i);
+    }
 }
