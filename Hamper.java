84cb4e3431433ab0830d3e2a26b8f69910ba29bf
jamgamm <92126341+jamgamm@users.noreply.github.com>
2022-04-05 23:18:14 -0600

    Update Hamper.java

--- Hamper.java
+++ Hamper.java
@@ -1,5 +1,24 @@
 package edu.ucalgary.ensf409
 
 public class Hamper {
+  private NutrionalProfile nutritionalNeedsFamily;
+  private LinkedList<Food> foodList = new LinkedList<Food>();
+  
+  public Hamper(NutritionalProfile familyNeeds){
+    this.nutritionalNeedsFamily = familyNeeds;
+  }
+  
+  public void addToFoodList(Food foodItem){
+  
+  }
+  
+  public NutritionaProfile getNutritionalNeedsFamily(){
+    return nutritionalNeedsFamily;
+  
+  }
+  
+  public void setNutritionalNeedsFamily(NutritionalProfile nutritionalNeedsFamily){
+  
+  }
 
 }
