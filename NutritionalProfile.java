edaeb66b944330fb9d100275c15fe78caf70cac6
slck16 <84081500+slck16@users.noreply.github.com>
2022-04-05 23:19:51 -0600

    Update NutritionalProfile.java

--- NutritionalProfile.java
+++ NutritionalProfile.java
@@ -1,5 +1,38 @@
 package edu.ucalgary.ensf409
 
 public class NutritionalProfile {
+  private int grainContent;
+  private int fvContent;
+  private int proContent;
+  private int other;
+  private int calories;
+  
+  public NutritionalProfile (int wholeGrains, int fruitVeggies, int protein, int other, int calories){
+    this.grainContent = wholeGrains;
+    thisfvContent = fruitVeggies;
+    this.proContent = protein;
+    this.other = other;
+    this.calories = calories;
+  }
 
-}
+  public int getGrainContent(){
+    return this.grainContent;
+  }
+
+  public int fvContent(){
+    return this.fvContent;
+  }
+
+  public int proContent(){
+    return this.proContent;
+  }
+
+  public int other(){
+   return this.other;
+  }
+
+  public int calories(){
+   return this.calories;
+  }
+ }
+
