88420463e845e73881c2943117b71cd44b0051cf
JanaAfifi <101433549+JanaAfifi@users.noreply.github.com>
2022-04-05 23:45:05 -0600

    Update NutritionalProfileClient.java

--- NutritionalProfileClient.java
+++ NutritionalProfileClient.java
@@ -2,5 +2,30 @@ package edu.ucalgary.ensf409;
 
 public enum NutritionalProfileClient{
   
+  ADULTMALE {
+        public String asString() {
+            return "Adult Male";
+        }
+    },
+    
+    ADULTFEMALE {
+        public String asString() {
+            return "Adult Female";
+        }
+    },
   
+    CHILDABOVE {
+        public String asString() {
+            return "Child Above";
+        }
+    },
+  
+    CHILDBELOW {
+        public String asString() {
+            return "Child Under";
+        }
+    
+    };
+    public abstract String asString();
+  //do we keep it as strong or string[]
 }
