7f9fcab4c39b8e80b286b7b86a1046655dbcd89a
JanaAfifi <101433549+JanaAfifi@users.noreply.github.com>
2022-04-05 23:19:56 -0600

    Update Family.java

--- Family.java
+++ Family.java
@@ -1,5 +1,13 @@
-package edu.ucalgary.ensf409
+package edu.ucalgary.ensf409;
 
 public class Family {
 
+  private int adultMale;
+  private int adultFemale;
+  private int childAbove;
+  private int chidBelow;
+  private LinkedList<Client> familyList;
+  private Hamper hamper;
+  private NutritionalProfile nutritionalNeedsFamily;
+  
 }
