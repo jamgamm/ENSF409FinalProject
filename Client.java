c2a79878b0162f91582a86b3584d06e7bd8c8266
amneetd <101241102+amneetd@users.noreply.github.com>
2022-04-05 23:21:17 -0600

    Update Client.java

--- Client.java
+++ Client.java
@@ -1,5 +1,23 @@
-package edu.ucalgary.ensf409
+package edu.ucalgary.ensf409;
+
+import java.io.*;
+import java.util.*;
 
 public class Client {
+  
+  private String category;
+  private final NutrionalProfile NUTRITIONALPROFILE;
+  
+  public Client(String category){
+    //write implementation
+  }
+  
+  public String getCategory(){
+    return this.category;
+  }
+  
+  public NutrionalProfile getNutrionalProfile(){
+    return this.NUTRITIONALPROFILE;
+  }
 
 }
