5d2baf12710c3a66b5b4aaf45a37f1fc1e9d339f
slck16 <84081500+slck16@users.noreply.github.com>
2022-04-05 23:23:07 -0600

    Update Inventory.java

--- Inventory.java
+++ Inventory.java
@@ -1,5 +1,17 @@
 package edu.ucalgary.ensf409
 
 public class Inventory {
+  private LinkedList<Food> inventoryList = new LinkedList<Food>();
+  
+  public Inventory (String url, String user, String password){
+  
+  }
 
+  public remove (String itemID){
+  
+  }
+  
+  public String getItem (String itemID){
+  
+  }
 }
