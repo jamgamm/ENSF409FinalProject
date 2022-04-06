e57738b287435436bf5db34fd322897da4fe0400
amneetd <101241102+amneetd@users.noreply.github.com>
2022-04-05 23:28:25 -0600

    Update OrderForm.java

--- OrderForm.java
+++ OrderForm.java
@@ -1,5 +1,22 @@
 package edu.ucalgary.ensf409;
 
+import java.util.*;
+import java.io.*;
+
 public class OrderForm{
+  
+  private ArrayList<Family> request;
+  
+  public void writeToTextFile() throws FileIOException{
+    //write implementation
+  }
+  
+  public static void getUserInput(String args[]){
+    //write implementation
+  }
+  
+  public boolean checkValid(Hamper finalHamper) throws IllegalArgumentException{
+    //write implementation
+  } 
 
 }
