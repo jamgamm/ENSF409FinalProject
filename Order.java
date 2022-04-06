62aca15b4d23d4629cc189095afb5a640e22b411
jamgamm <92126341+jamgamm@users.noreply.github.com>
2022-04-05 23:25:13 -0600

    Update Order.java

--- Order.java
+++ Order.java
@@ -1,5 +1,17 @@
 package edu.ucalgary.ensf409
 
 public class Order {
+  private LinkedList[] hamperList;
+  
+  public Order(){
+  
+  }
+  
+  public Hamper findBestCombo(){
+    
+    //implementation
+  
+    return bestHamper;
+  }
 
 }
