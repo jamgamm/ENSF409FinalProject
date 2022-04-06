63296dd5478ac3381955fc5779fa34395aad11c0
amneetd <101241102+amneetd@users.noreply.github.com>
2022-04-05 23:17:29 -0600

    Update Food.java

--- Food.java
+++ Food.java
@@ -1,5 +1,33 @@
 package edu.ucalgary.ensf409
 
+import java.util.*;
+import java.io.*;
+
+
 public class Food {
 
+  private final String ID;
+  private final String NAME;
+  private NutritionalProfile nutritionalProfile;
+  
+  public Food(String ID, String name){
+    //write implementation
+  }
+  
+  public String getID(){
+    return this.ID;
+  }
+  
+  public String getName(){
+    return this.NAME;
+  }
+  
+  public NutrionalProfile(){
+    return this.nutritionalProfile;
+  }
+  
+  public void setNutrionalProfile(String profile){
+    //write implementation
+  }
+  
 }
