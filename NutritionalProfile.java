945442400498aab531e44bc655d243a98c728b62
jamgamm <92126341+jamgamm@users.noreply.github.com>
2022-04-10 21:01:21 -0600

    Update NutritionalProfile.java

--- NutritionalProfile.java
+++ NutritionalProfile.java
@@ -1,18 +1,30 @@
 package edu.ucalgary.ensf409
 
 public class NutritionalProfile {
+  private final int ID;
+  private final String NAME;
   private int grainContent;
   private int fvContent;
   private int proContent;
   private int other;
   private int calories;
   
-  public NutritionalProfile (int wholeGrains, int fruitVeggies, int protein, int other, int calories){
+  public NutritionalProfile (int itemID, string itemName, int wholeGrains, int fruitVeggies, int protein, int other, int calories){
+    this.ID = itemID;
+    this.NAME = itemName;
     this.grainContent = wholeGrains;
     this.fvContent = fruitVeggies;
     this.proContent = protein;
     this.other = other;
     this.calories = calories;
+  }
+  
+  public String getID(){
+    return this.ID;
+  }
+  
+  public String getName(){
+    return this.NAME;
   }
 
   public int getGrainContent(){
