0e4562e56aa5a00e0332da27003bbcef3909904b
jamgamm <92126341+jamgamm@users.noreply.github.com>
2022-04-14 01:34:19 -0600

    Update Client.java

--- Client.java
+++ Client.java
@@ -5,20 +5,62 @@ import java.util.*;
 
 public class Client {
   
-  private String category;
-  private final NutrionalProfile NUTRITIONALPROFILE;
+  private int ID;
+  private String NAME;
+  private int grainContent;
+  private int fvContent;
+  private int proContent;
+  private int otherContent;
+  private int calories;
   
-  public Client(String category) throws IllegalArgumentException{
-    this.category = category;
-    //I think this one uses the enum right?
+  public Client (int itemID, String itemName, int wholeGrains, int fruitVeggies, int protein, int other, int calories){
+    this.ID = itemID;
+    this.NAME = itemName;
+    this.grainContent = wholeGrains;
+    this.fvContent = fruitVeggies;
+    this.proContent = protein;
+    this.otherContent = other;
+    this.calories = calories;
+  }
+
+  public Client (int wholeGrains, int fruitVeggies, int protein, int other, int calories){
+    this.grainContent = wholeGrains;
+    this.fvContent = fruitVeggies;
+    this.proContent = protein;
+    this.otherContent = other;
+    this.calories = calories;
+  }
+
+  public Client(){
+    
+  }
+
+  public int getID(){
+    return this.ID;
   }
   
-  public String getCategory(){
-    return this.category;
+  public String getName(){
+    return this.NAME;
   }
-  
-  public NutrionalProfile getNutrionalProfile(){
-    return this.NUTRITIONALPROFILE;
+
+  public int getGrainContent(){
+    return this.grainContent;
+  }
+
+  public int getFVContent(){
+    return this.fvContent;
+  }
+
+  public int getProContent(){
+    return this.proContent;
+  }
+
+  public int getOther(){
+   return this.otherContent;
+  }
+
+  public int getCalories(){
+   return this.calories;
   }
 
 }
