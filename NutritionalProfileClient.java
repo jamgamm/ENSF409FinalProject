aeea352610d728475c685a00fc42ae6567d3dcfd
jamgamm <92126341+jamgamm@users.noreply.github.com>
2022-04-14 01:37:04 -0600

    Update NutritionalProfileClient.java

--- NutritionalProfileClient.java
+++ NutritionalProfileClient.java
@@ -3,29 +3,49 @@ package edu.ucalgary.ensf409;
 public enum NutritionalProfileClient{
   
   ADULTMALE {
-        public String asString() {
-            return "Adult Male";
+        public int[] theProfile() {
+            NutritionalProfile theInfo = ourInven.getClientMap().get(1);
+            int grains = theInfo.getGrainContent();
+            int fv = theInfo.getFVContent();
+            int protein = theInfo.getProContent();
+            int other = theInfo.getOther();
+            int calories = theInfo.getCalories();
+            int[] profile = {grains, fv, protein, other, calories};
+            return profile;
+            //int[] profile = {400, 700, 650, 750, 2500};
         }
     },
     
     ADULTFEMALE {
-        public String asString() {
-            return "Adult Female";
+        public int[] theProfile() {
+            NutritionalProfile theInfo = ourInven.getClientMap().get(2);
+            int grains = theInfo.getGrainContent();
+            int fv = theInfo.getFVContent();
+            int protein = theInfo.getProContent();
+            int other = theInfo.getOther();
+            int calories = theInfo.getCalories();
+            int[] profile = {grains, fv, protein, other, calories};
+            //int[] profile = {320, 560, 520, 600, 2000};
+            return profile;
         }
     },
   
     CHILDABOVE {
-        public String asString() {
-            return "Child Above";
+        public int[] theProfile() {
+            int[] profile = {462, 726, 682, 330, 2200};
+            return profile;
+            //return new NutritionalProfile(3, "Child Over 8", 462, 726, 682, 330, 2200);
         }
     },
   
     CHILDBELOW {
-        public String asString() {
-            return "Child Under";
+        public int[]theProfile() {
+            int[] profile = {294, 462, 434, 210, 1400};
+            return profile;
+            //return new NutritionalProfile(4, "Child Under 8", 294, 462, 434, 210, 1400);
         }
     
     };
-    public abstract String asString();
-  //do we keep it as strong or string[]
+    public abstract int[] theProfile();
+    Inventory ourInven = new Inventory();
 }
