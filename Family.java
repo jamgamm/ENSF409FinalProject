cc0a31e6ba0be9876baa6aa2905ca5bffde6446d
jamgamm <92126341+jamgamm@users.noreply.github.com>
2022-04-14 01:34:58 -0600

    Update Family.java

--- Family.java
+++ Family.java
@@ -1,22 +1,24 @@
 package edu.ucalgary.ensf409;
 
+import java.util.LinkedList;
+
 public class Family {
 
   private int adultMale;
   private int adultFemale;
   private int childAbove;
-  private int chidBelow;
-  private LinkedList<Client> familyList;
+  private int childBelow;
+  //private LinkedList<Client> familyList;
   private Hamper hamper;
-  private NutritionalProfile nutritionalNeedsFamily;
+  private Client nutritionalNeedsFamily;
   
-  Public String Family(int adultMale, int adultFemale, int childAbove, int childBelow, Hamper hamper)throws IllegalArgumentException{
+  public Family(int adultMale, int adultFemale, int childAbove, int childBelow /*Hamper hamper*/)throws IllegalArgumentException{
 
     this.adultMale = adultMale;
     this.adultFemale = adultFemale;
     this.childAbove = childAbove;
-    this.chidBelow = childBelow;
-    this.hamper = hamper;
+    this.childBelow = childBelow;
+    this.hamper = new Hamper(getWeeklyFamilyNutritionalNeeds());
     
   }
    public int[] getFamilyMembers() { ////finish the method???
@@ -24,20 +26,37 @@ public class Family {
 	   return familyMembers;
 }
   
-  public NutritionalProfile getFamilyNutritionalNeeds(){ ////finish the method???
-	  return ;
+  public Client getWeeklyFamilyNutritionalNeeds(){ ////finish the method???
+      int totalGrain = this.adultMale*NutritionalProfileClient.ADULTMALE.theProfile()[0]
+                     + this.adultFemale*NutritionalProfileClient.ADULTFEMALE.theProfile()[0]
+                     + this.childBelow*NutritionalProfileClient.CHILDBELOW.theProfile()[0]
+                     + this.childAbove*NutritionalProfileClient.CHILDABOVE.theProfile()[0];
+      int totalFV = this.adultMale*NutritionalProfileClient.ADULTMALE.theProfile()[1]
+                    + this.adultFemale*NutritionalProfileClient.ADULTFEMALE.theProfile()[1]
+                    + this.childBelow*NutritionalProfileClient.CHILDBELOW.theProfile()[1]
+                    + this.childAbove*NutritionalProfileClient.CHILDABOVE.theProfile()[1];
+      int totalPro = this.adultMale*NutritionalProfileClient.ADULTMALE.theProfile()[2]
+                    + this.adultFemale*NutritionalProfileClient.ADULTFEMALE.theProfile()[2]
+                    + this.childBelow*NutritionalProfileClient.CHILDBELOW.theProfile()[2]
+                    + this.childAbove*NutritionalProfileClient.CHILDABOVE.theProfile()[2];
+      int totalOther = this.adultMale*NutritionalProfileClient.ADULTMALE.theProfile()[3]
+                    + this.adultFemale*NutritionalProfileClient.ADULTFEMALE.theProfile()[3]
+                    + this.childBelow*NutritionalProfileClient.CHILDBELOW.theProfile()[3]
+                    + this.childAbove*NutritionalProfileClient.CHILDABOVE.theProfile()[3];
+      int totalCal = this.adultMale*NutritionalProfileClient.ADULTMALE.theProfile()[4]
+                    + this.adultFemale*NutritionalProfileClient.ADULTFEMALE.theProfile()[4]
+                    + this.childBelow*NutritionalProfileClient.CHILDBELOW.theProfile()[4]
+                    + this.childAbove*NutritionalProfileClient.CHILDABOVE.theProfile()[4];
+      this.nutritionalNeedsFamily = new Client(5, "Family", totalGrain*7, totalFV*7, totalPro*7, totalOther*7, totalCal*7);
+      System.out.println("Total Grain: " + totalGrain + "Total FV: "+ totalFV);
+	  return nutritionalNeedsFamily;
   }
   
-	public int[] convertPercent(){
-		//access the nutritional profile for the desried person
-		//then convert each part of the profile (grains fv etc) from percent to a number
-		//store all these numbers into an int array
-	}
-	
-  public NutritionalProfile calculateWeeklyFamilyNutrtionalNeeds(){ ////finish the method???
-    //use the getter for family nutritional needs
-    //then multiply by 7
-    return ;
+  public Hamper getHamper(){
+    return this.hamper;
   }
+
+	
   
 }
+
