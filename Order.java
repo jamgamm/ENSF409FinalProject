ebee2693be73cc92c7e37d3ec330037c116f0224
jamgamm <92126341+jamgamm@users.noreply.github.com>
2022-04-14 01:39:48 -0600

    Update Order.java

--- Order.java
+++ Order.java
@@ -1,59 +1,57 @@
-package edu.ucalgary.ensf409
+package edu.ucalgary.ensf409;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Order {
   //public LinkedList[] hamperList;
+  private Hamper myHamper;
+  private List<NutritionalProfile> theCombo = new ArrayList<NutritionalProfile>();
+  private Client bestHamper = new Client();
   
-  public Order(){
+  public Order(Hamper theHamper){
+    this.myHamper = theHamper;
   //might need a family object
   
   }
   
-  public Hamper findBestCombo(LinkedList[] hamperList){
-    // have a score system to determine best hamper combination
-    // if a hamper does not reach the requirement for a particular nutrition then the score is  and hamper should be dropped
-    // eg grainScore = 1; fvScore = 1; proScore = 2; otherScore = 2; tCaloriesScore = ; 
-    //score could be the amount that is over the minimum needed
-    //implementation
-    
-    // maybe a different building algorithm for each order
-    // depending on number of family members
-    // approximate an amount of food items to be chosen
-    // if too small or too large
-    // increment or decrement the amount and construct
-    // a new list of hampers
-    
-    // pseudocode
-    // get number of family members e.g 1
-    // determine number of food items
-    // from items available in inventory
-    // build hampers with said number of items
-    // for each hamper, keep record of the nutritional value
-    // add hamper to list
-    // once all possible hampers have been made
-    // compare and choose the best
-    
-    ArrayList<Integer> scoresList = new ArrayList<>();
-    NutritionalProfile want = hamperList.get(i).getNutritionalNeedsFamily();
-    int grains = want.getGrainContent();
-    int fv = want.getFVContent();
-    int pro = want.getProContent();
-    int other = want.getOtherContent();
-    for (int i = 0; i < hamperList.size(); i++)
-    {
-      NutritionalProfile have = hamperList.get(i).getHamperProfile();
-      int grainsH = have.getGrainContent();
-      int fvH = have.getFVContent();
-      int proH = have.getProContent();
-      int otherH = have.getOtherContent();
-      
-      int difference = (grainsH - grains) + (fvH - fv) + (proH - pro) + (otherH - other);
-      scoresList.add(difference);
-     }
-    
-    ArrayList<Integer> scoresSortedList = scoresList.sort();
-    int index = scoresList.indexOf(scoresSortedList.get(0));
-    
-    return hamperList.get(index);
+  public void bestCombo(List<Client> generated){
+
+    int bestIndex = 0;
+    int bestCalories = generated.get(0).getCalories();
+    int checkLess = 0;
+    int checkGrain = 0;
+    int checkFV = 0;
+    int checkPro = 0;
+    int checkOther = 0;
+    Client familyNeeds = myHamper.getNutritionalNeedsFamily();
+    for(int i = 1; i<generated.size();i++){
+      Client currProfile = generated.get(i);
+      checkLess = currProfile.getCalories() - familyNeeds.getCalories();
+      checkGrain = currProfile.getGrainContent();
+      checkFV = currProfile.getFVContent();
+      checkPro = currProfile.getProContent();
+      checkOther = currProfile.getOther();
+      if(currProfile.getCalories()<bestCalories && checkLess>=0 && checkGrain>=familyNeeds.getGrainContent()
+      && checkFV>=familyNeeds.getFVContent()&&checkPro>=familyNeeds.getProContent()&&checkOther>=familyNeeds.getOther()){
+        bestCalories = generated.get(i).getCalories();
+        bestIndex = i;
+      }
+    }
+    System.out.println(bestCalories- myHamper.getNutritionalNeedsFamily().getCalories());
+
+    this.bestHamper = myHamper.getHamperProfile().get(bestIndex);
+    this.theCombo = myHamper.getPossible().get(bestIndex);
+
+
+
   }
 
+  public List<NutritionalProfile> getBestCombo(){
+    return theCombo;
+  }
+
+  public Client getBestHamper(){
+    return bestHamper;
+  }
 }
