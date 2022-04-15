82e50fd56da27aee4c76ca076f11043dc70e38fa
jamgamm <92126341+jamgamm@users.noreply.github.com>
2022-04-14 20:53:12 -0600

    Update OrderForm.java

--- OrderForm.java
+++ OrderForm.java
@@ -1,52 +1,68 @@
 package edu.ucalgary.ensf409;
 
 import java.util.*;
-import java.util.List;
 import java.io.*;
-import java.time.LocalDate;
 
 
 public class OrderForm{
   
     //request will hold mutiple families
-  private ArrayList<Family> request;
+  private ArrayList<Family> request = new ArrayList<Family>();
   private Family fam1;
   private int numOfFamilies = 0;
-  private Hamper goodHamper; //I added this private var
+  private Hamper goodHamper;
+  private List<Hamper> allHampers = new ArrayList<Hamper>();
 
-  public OrderForm(int female, int male, int over, int under) throws IllegalArgumentException, FileNotFoundException{
-      //this is just for 1 family
-      //maybe for multiple families use for loop?
-        this.fam1 = new Family(male, female, over, under);
-        this.goodHamper = fam1.getHamper();
-        //request.add(fam1);
-        numOfFamilies++;
-        writeToTextFile();
+  public OrderForm(int female, int male, int over, int under) throws IllegalArgumentException, FileNotFoundException, OrderCannotBeValidatedException{
+      fam1 = new Family(male, female, over, under);
+      request.add(fam1);
+      numOfFamilies++;
+      this.goodHamper = fam1.getHamper();
+      allHampers.add(goodHamper);
+      //System.out.println("Hampers made");
   }
 
-  public Family getFamily(){
-    return fam1;
+  public OrderForm(){
+
+  }
+
+  public ArrayList<Family> getFamily(){
+    return request;
+  }
+
+  public List<Hamper> getAllHampers(){
+    return allHampers;
+  }
+
+  public void addHamper(int female, int male, int over, int under) throws IllegalArgumentException, OrderCannotBeValidatedException{
+    Family nextFamily = new Family(male, female, over, under);
+    request.add(nextFamily);
+    nextFamily.makeHamper();
+    allHampers.add(nextFamily.getHamper());
+    //System.out.println("Hamper created");
   }
   
-  public void writeToTextFile() throws FileNotFoundException{
+  public void writeToTextFile(int orderNumber, String mobility) throws FileNotFoundException{
     //an example of the final output: https://d2l.ucalgary.ca/d2l/le/content/425075/viewContent/5235566/View 
-    //List<Client> ourFood = hamper1.getHamperProfile();
-    Order order1 = goodHamper.getOrder();
-    List<NutritionalProfile> best = order1.getBestCombo();
     try {
-      FileWriter out = new FileWriter("output.txt");
-      int[] members = fam1.getFamilyMembers();
-      String familyMembers = new String(members[0]+"male, "+members[1]+"female, "+members[2]+"child above 8, "+members[3]+"child below 8");
-      out.write("Name: " + "\t\n" + "Date: " + LocalDate.now() + "\t\n" + "Original Request: " + familyMembers + "\n");
-      for(int i =0; i<best.size(); i++){
-        out.write("ID: " + best.get(i).getID()+ " Name: "+ best.get(i).getName()+"\n");
+      FileWriter out = new FileWriter("OrderForm" +orderNumber+".txt");
+      for(int i = 0; i<getFamily().size(); i++){
+        Family currFam = getFamily().get(i);
+        int [] members = currFam.getFamilyMembers();
+        String familyMembers = new String(members[0]+" male, "+members[1]+" female, "+members[2]+" child above 8, "+members[3]+" child below 8");
+        out.write("Name: " + "\t\n" + "Date: " + "\t\n" + "Original Request: " + familyMembers + "\n");
+        out.write("Mobility Needed: "+ mobility+"\n");
+        Order currHamper = getAllHampers().get(i).getOrder();
+        out.write("Excess: "+currHamper.getExcess()+"\n");
+        List<NutritionalProfile> bestFoods = currHamper.getBestCombo();
+        for(int j=0; j<bestFoods.size(); j++){
+          out.write("ID: " + bestFoods.get(j).getID()+ " Name: "+ bestFoods.get(j).getName()+"\n");
+        }
       }
             
             
        out.close();
        }
-        
-        
         //check for io or regular exceptions (and print if necessary)
         catch (IOException e){
             System.err.println("IOException: " + e.getMessage());
