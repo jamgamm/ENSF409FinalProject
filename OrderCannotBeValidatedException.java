b685d955225f710b11e4e38586dc674ca792b40c
slck16 <84081500+slck16@users.noreply.github.com>
2022-04-15 14:29:00 -0600

    Update OrderCannotBeValidatedException.java

--- OrderCannotBeValidatedException.java
+++ OrderCannotBeValidatedException.java
@@ -1,7 +1,16 @@
 package edu.ucalgary.ensf409;
 
+/**
+ * An exception class that throws an exception when a hamper cannot be made
+ * checks for this exception are made whenever a hamper object is being made
+ * @author Jana Afifi, Amneet Deol, Jam Ivy Gammuac, Shanelle Li Chit Khim
+ * @version 1.3
+ * @since 1.0 
+ */
+
 public class OrderCannotBeValidatedException extends Exception{
+    /** Constructor **/
     public OrderCannotBeValidatedException(){
         super("Order cannot be validated");
     }
-}
+} // End of class declaration
