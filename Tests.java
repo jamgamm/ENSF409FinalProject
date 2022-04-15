76dc994025eaedfe1437b11f11141dc06e5745fe
jamgamm <92126341+jamgamm@users.noreply.github.com>
2022-04-15 02:08:57 -0600

    Update Tests.java

--- Tests.java
+++ Tests.java
@@ -6,12 +6,16 @@ import java.io.*;
 import java.util.*;
 //any other libraries we need to import
 
-public class Test {
-  private Hamper exampleHamper = null;
-  private Family sampleFamily = new Family(1,2,3,4, exampleHamper);
-  private Client sampleClient = new Client("Adult male");
+public class Tests {
+  //private Client sampleClient = new Client(400, 700, 650, 750, 2500);
+  private Hamper sampleHamper = null;
+  private Family sampleFamily = null;
+  private Client sampleClient = new Client(0, "Adult male", 0, 0, 0, 0, 0);
+  private NutritionalProfile sampleProfile = new NutritionalProfile(0,0,0,0,0);
   private Hamper badHamper = null;
-  private Food testFood = new Food("12", "banana");
+  private NutritionalProfile testFood = new NutritionalProfile(1, "Banana", 0, 100, 0, 0, 100);
+  //private Inventory sampleInventory = new Inventory();
+  private Order sampleOrder = null;
   
   /* this section is for testing the methods in the family class 
   the first 4 tests under this comment are all for testing methods in family class */
@@ -20,7 +24,8 @@ public class Test {
   @Test
   public void testFamilyConstructorFull(){
     //test the constructor when its given valid data( 1 adult male/female, 1 child under/over 8)
-    Family sampleFamily = new Family(1,2,3,4, exampleHamper);
+    sampleFamily = new Family(1,1,1,1);
+
     assertNotNull("Family constructor did not create an object when given valid data", sampleFamily);
   }
   
@@ -28,112 +33,94 @@ public class Test {
   @Test
   public void testFamilyConstructorSingle(){
     //test the constructor when its given valid data(1 adult male)
-    Family sampleFamily = new Family(1,0,0,0, exampleHamper);
+    sampleFamily = new Family(1,0,0,0);
+
     assertNotNull("Family constructor did not create an object when given valid data", sampleFamily);
   }
   
   @Test
   public void testFamilyGetters(){
     //test getters in family class
-    int[] sampleGetMembers = sampleFamily.getFamilyMembers();
-    int[] expectedMembers = [1,2,3,4];
+    sampleFamily = new Family(1,0,0,0);
+    int sampleGetMembers = sampleFamily.getFamilyMembers()[0];
+    int expectedMembers = 1;
     assertNotNull("Method getFamilyMembers did not return an int array", sampleGetMembers);
     assertEquals("Method getFamilyMembers did not return the expected result: ", expectedMembers, sampleGetMembers);
-    NutritionalProfile sampleGetFamilyNeeds = sampleFamily.getFamilyNutritionalNeeds();
-    assertNotNull("Method getFamilyNutritionalNeeds did not return a NutritionalProfile object", sampleGetFamilyNeeds);  
   }
   
   @Test
   public void testFamilyCalculation(){
     //test the calculation for weekly nutritional needs of the family
-    int[][] sampleCalculation = sampleFamily.calculateWeeklyFamilyNutritionalNeeds();
-    int[][] expectedCalculation = //this will be some sort of value we calculate ourselves
-    assertEquals("Method calculateWeeklyFamilyNutritionalNeeds did not return the expected result: ", expectedCalculation, sampleCalculation);
+    sampleFamily = new Family(1,0,0,0);
+    sampleClient = new Client(400, 700, 650, 750, 2500);
+    sampleFamily.setWeeklyFamilyNutritionalNeeds(sampleClient);
+    Client sampleCalculation = sampleFamily.getWeeklyFamilyNutritionalNeeds();
+    int sample = sampleCalculation.getGrainContent();
+    int expectedCalculation = 400;
+    assertEquals("Method calculateWeeklyFamilyNutritionalNeeds did not return the expected result: ", expectedCalculation, sample);
   }
 
   /* this section is for testing the methods related to the client class */
   @Test
   public void testClientConstructor(){
     //test the constructor when given a client of category adult male
-    Client sampleClient = new Client("Adult male");
+    sampleClient = new Client(1, "Adult male", 16, 28, 26, 30, 2500);
     assertNotNull("Client constructor did not create an object when given valid data", sampleClient);
   }
   
   @Test
   public void testClientGetters(){
     //test the getters for the client class
-    String sampleCategory = sampleClient.getCategory();
+    String sampleCategory = sampleClient.getName();
     String expectedCategory = "Adult male";
     assertEquals("Method getCategory did not return the expected result: ", expectedCategory, sampleCategory);
-    NutritionalProfile sampleProfile = sampleClient.getNutritionalProfile();
-    assertNotNull("Method getNutritionalProfile did not return a NutritionalProfile object", sampleProfile);  
   }
 
   /* this section is for testing the methods related to the Nutritional Profile class */
   @Test
   public void testProfileConstructor(){
     //test the constructor for nutrtional profile given all valid int values
-    NutritionalProfile sampleProfile = new NutritionalProfile(400,700,650,750,2500);
+    sampleProfile = new NutritionalProfile(0,80,10,10,120);
     assertNotNull("NutritionalProfile constructor did not create an object when given valid data", sampleProfile);
   }
   
   @Test
   public void testProfileGetters(){
     //test the getters for the nutritional profile class
+    sampleProfile = new NutritionalProfile(0,80,10,10,120);
     int sampleGrains = sampleProfile.getGrainContent();
-    int expectedGrains = 400;
+    int expectedGrains = 0;
     assertEquals("Method getGrainContent did not return the expected result: ", expectedGrains, sampleGrains);
     int sampleFV = sampleProfile.getFVContent();
-    int expectedFV = 700;
+    int expectedFV = 96;
     assertEquals("Method getFVContent did not return the expected result: ", expectedFV, sampleFV);
     int samplePro = sampleProfile.getProContent();
-    int expectedPro = 650;
+    int expectedPro = 12;
     assertEquals("Method getProContent did not return the expected result: ", expectedPro, samplePro);
     int sampleOther = sampleProfile.getOther();
-    int expectedOther = 750;
+    int expectedOther = 12;
     assertEquals("Method getOther did not return the expected result: ", expectedOther, sampleOther);
     int sampleCalories = sampleProfile.getCalories();
-    int expectedCalories = 2500;
+    int expectedCalories = 120;
     assertEquals("Method getCalories did not return the expected result: ", expectedCalories, sampleCalories);
   }
   
-  /* this section is for testing the methods related to the Food class */
-  
-  @Test
-  public void testFoodConstructor(){
-    //test the constructor when given valid id and name
-    Food sampleFood = new Food("1", "Tomato Sauce, jar");
-    assertNotNull("Food constructor did not create an object when given valid data", sampleFood);
-  }
-  
-  @Test
-  public void testFoodGetters(){
-    //test getters for food class
-    String sampleID = sampleFood.getID();
-    String expectedID = "1";
-    assertEquals("Method getID did not return the expected result: ", expectedID, sampleID);
-    String sampleName = sampleFood.getName();
-    String expectedName = "Tomato Sauce, jar";
-    assertEquals("Method getName did not return the expected result: ", expectedName, sampleName);
-    NutritionalProfile sampleProfile = sampleFood.getNutritionalProfile();
-    assertNotNull("Method getNutritionalProfile did not return a NutritionalProfile object", sampleProfile); 
-  }
   
   /* this section is for testing the methods related to the Inventory class*/
   
-  @Test
+  /*@Test
   public void testInventoryGetter(){
     //test the getter for inventory class
-    String sampleInventoryID = sampleInventory.getItem();
-    String expectedInventoryID = "1";
+    int sampleInventoryID = sampleInventory.getItem(1).getID();
+    int expectedInventoryID = 1;
     assertEquals("Method getItem did not return the expected result: ", expectedInventoryID, sampleInventoryID);
   }
     
   @Test
   public void testRemove(){
     //test the remove function by trying to remove tomato sauce
-    sampleInventory.remove("1", "Tomato Sauce, jar");
-    String sampleInventoryID = sampleInventory.getItem();
+    sampleInventory.remove(1);
+    int sampleInventoryID = sampleInventory.getItem(1).getID();
     String expectedInventoryID = null;
     assertEquals("Method remove did not return the expected result: ", expectedInventoryID, sampleInventoryID);
   }
@@ -143,75 +130,49 @@ public class Test {
   @Test
   public void testHamperConstructor(){
     //test the constructor when given a valid nutrtional profile
-    Hamper sampleHamper = new Hamper(new NutritionalProfile(400,700,650,750,2500));
+    Client sampleClient = new Client(400, 700, 650, 750, 2500);
+    Hamper sampleHamper = new Hamper(sampleClient);
     assertNotNull("Hamper constructor did not create an object when given valid data", sampleHamper);
   }
   
-  @Test
-  public void testAdd(){
-    //test the add to food list method by adding tomato sauce
-    sampleHamper.addToFoodList(sampleFood);
-    Food theFood = foodList.last();
-    String theFoodID = theFood.getID();
-    String expectedFoodID = "1";
-    assertEquals("Method addToFoodList did not return the expected result: ", expectedFoodID, theFoodID);
-  }
-  
-  @Test
-  public void testSetNutriNeeds(){
-    //test setter for setting the nutritional needs
-    sampleHamper.setNutritionalNeedsFamily(new NutrionalProfile(400,700,650,750,2501));
-    NutritionalProfile sampleNutriProfile = sampleHamper.getNutritionalNeedsFamily;
-    assertNotNull("Method setNutritionalNeedsFamily did not create a NutritionalProfile object", sampleNutriProfile);
-  }
   
   @Test
   public void testGetNutriNeeds(){
     //test getter
-    NutritionalProfile sampleNutriProfile = sampleHamper.getNutritionalNeedsFamily;
-    assertNotNull("Method getNutritionalNeedsFamily did not return a NutritionalProfile object", sampleNutriProfile);
+    Hamper sampleHamper = new Hamper(sampleClient);
+
+    Client sampleNutriProfile = sampleHamper.getNutritionalNeedsFamily();
+    assertNotNull("Method getNutritionalNeedsFamily did not return a Client object", sampleNutriProfile);
   }
    
-  /* this section will test the methods related to the order form class */
-  
-  @Test
-  public void testCheckValid(){
-    boolean expectedValidity = true;
-    boolean sampleValidity = sampleOrderForm.checkValid(sampleHamper);
-    assertEquals("Method checkValid did not return the expected result: ", expectedValidity, sampleValidity);
     
  /* this section will test the methods related to the order class */
   
   @Test
   public void testOrderConstructor(){
-    Order sampleOrder = new Order(sampleFamily);
+    Order sampleOrder = new Order(sampleHamper);
     assertNotNull("Order constructor did not create an object when given valid data", sampleOrder);
   }
     
   @Test
   public void testFindBestCombo(){
-    Hamper sampleBestCombo = sampleOrder.findBestCombo();
-    Hamper expectedBestCombo = //something ;
-    assertEquals("Method findBestCombo did not return the expected result: ", expectedBestCombo, sampleBestCombo);
+    sampleClient = new Client(2741, 700, 650, 750, 2500);
+    sampleHamper = new Hamper(sampleClient);
+    sampleOrder = new Order(sampleHamper);
+    sampleOrder.setBestHamper(sampleClient);
+    Client sampleBestCombo = sampleOrder.getBestHamper();
+    int bestCombo = sampleBestCombo.getGrainContent();
+    int expectedBestCombo = 2741;
+    assertEquals("Method findBestCombo did not return the expected result: ", expectedBestCombo, bestCombo);
   }
     
   /* this section will test the exceptions for various methods */
     
   @Test
   public void testFamilyConstructorException(){
-    boolean passed = false;
+    boolean passed = true;
     //test that an illegal arguement exception is thrown when invalid data is given
     
-    try{
-      new Family("cat");
-    }
-    catch (IllegalArgumentException e){
-      passed = true;
-    }
-    catch (Exception e) { }
-        assertTrue("Family constructor did not throw IllegalArgumentException when given invalid data", passed);
-    
-    passed = true;
     try{
       new Family(1, 0, 0, 0);
     }
@@ -226,19 +187,10 @@ public class Test {
   @Test
   public void testClientConstructorException(){
     //test that an illegal arguement exception is thrown when invalid data is given
-    boolean passed = false;
-    try{
-      new Client(1);
-    }
-    catch (IllegalArgumentException e){
-      passed = true;
-    }
-    catch (Exception e) { }
-        assertTrue("Client constructor did not throw IllegalArgumentException when given invalid data", passed);
     
-    passed = true;
+    boolean passed = true;
      try{
-      new Client("Adult male");
+      new Client(1,"Adult male",16,28,26,30,2500);
     }
     catch (IllegalArgumentException e){
       passed = false;
@@ -246,88 +198,9 @@ public class Test {
     catch (Exception e) { }
         assertTrue("Client constructor throws IllegalArgumentException when given valid data", passed);
   } 
+
     
-  @Test
-  public void testCheckValid(){
-    //test that an illegal arguement exception is thrown when invalid data is given
-    boolean passed = false;
-    try{
-      sampleOrderForm.checkValid(badHamper);
-    }
-    catch (IllegalArgumentException e){
-      passed = true;
-    }
-    catch (Exception e) { }
-        assertTrue("Check valid method in Order Form class did not throw IllegalArgumentException when given invalid data", passed);
-    
-    passed = true;
-    NutritionalProfile clientNutritionalProfile = new NutritionalProfile(400,700,650,750,2500);
-    LinkedList<Food> foodList = new LinkedList<>();
-    foodList.add("1", "Tomato sauce, jar", new NutritionalProfile(400,700,650,750,2500));
-    Hamper validHamper = new Hamper(clientNutritionalProfile, foodList);
-    try{
-      sampleOrderForm.checkValid(validHamper);
-    }
-    catch (IllegalArgumentException e){
-      passed = false;
-    }
-    catch (Exception e) { }
-        assertTrue("Check valid method in Order Form class throws IllegalArgumentException when given valid data", passed);
-    
-  }
-    
-  @Test
-  public void testFoodConstructorException(){
-    //test that an illegal arguement exception is thrown when invalid data is given
-    boolean passed = false;
-    try{
-      new Food(11, "apple");
-    }
-    catch (IllegalArgumentException e){
-      passed = true;
-    }
-    catch (Exception e) { }
-        assertTrue("Food constructor did not throw IllegalArgumentException when given invalid data", passed);
-    
-    passed = true;
-    try{
-      new Food("11", "apple");
-    }
-    catch (IllegalArgumentException e){
-      passed = false;
-    }
-    catch (Exception e) { }
-        assertTrue("Food constructor throws IllegalArgumentException when given valid data", passed);
-    
-  }
-    
-  @Test
-  public void testAddToFoodListException(){
-    //test that an order cannot be validated exception is thrown when invalid data is given
-    boolean passed = true;
-    Inventory inv = new Inventory("url", "student", "ensf409");  // assume url is valid
- 
-    try{
-      sampleHamper.addToFoodList(testFood);
-    }
-    catch (IllegalArgumentException e){
-      passed = false;
-    }
-    catch (Exception e) { }
-        assertTrue("addToFoodList method throws IllegalArgumentException when given valid data", passed);
-    
-    inv.remove("12"); // removing testFood from the inventory
-    passed = false;
-    try{
-      sampleHamper.addToFoodList(testFood); // trying to add a food object that no longer exists
-    }
-    catch (IllegalArgumentException e){
-      passed = true;
-    }
-    catch (Exception e) { }
-        assertTrue("addToFoodList method does not throw IllegalArgumentException when given invalid data", passed);
-    
-  }
+}
     
     
   
