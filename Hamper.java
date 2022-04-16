0c7b3ec076b8969e50198bd7d285749fd850a747
jamgamm <92126341+jamgamm@users.noreply.github.com>
2022-04-16 03:49:30 -0600

    Update Hamper.java

--- Hamper.java
+++ Hamper.java
@@ -1,5 +1,11 @@
 package edu.ucalgary.ensf409;
-
+/**
+ * Class that creates possible hampers based on a family's nutritional needs
+ * Only creates possible hamper combinations for 1 family at a time
+ * @author Jana Afifi, Amneet Deol, Jam Ivy Gammuac, Shanelle Li Chit Khim
+ * @version 2.5
+ * @since 1.0 
+ */
 import java.util.*;
 
 public class Hamper {
@@ -8,42 +14,68 @@ public class Hamper {
   private List<Client> hamperProfile = new ArrayList<Client>();
   private Order theOrder = new Order(this);
   
+  /** Constructor **/
+  
+  /** 
+   * @param familyNeeds - the weekly nutritional needs of a family
+   */
   public Hamper(Client familyNeeds) throws IllegalArgumentException{
     this.nutritionalNeedsFamily = familyNeeds;
-    /*System.out.println("Family grains: " + nutritionalNeedsFamily.getGrainContent());
-    System.out.println("Family FV: " + nutritionalNeedsFamily.getFVContent());
-    System.out.println("Family Protein: " + nutritionalNeedsFamily.getProContent());
-    System.out.println("Family Other: " + nutritionalNeedsFamily.getOther());
-    System.out.println("Family calories: "+ nutritionalNeedsFamily.getCalories());*/
-    //this.possible = buildHamper();
-    //theOrder.bestCombo(hamperProfile);
   }
 
-  
+  /**
+   * this method will build the hamper that will be given to a single family
+   * First a new inventory object will be created to access the database.
+   * Through a for loop, the method create will be called up to 1000 times to create 
+   * possible combinations of hampersand each combination will be added to the list named possible.
+   * After all possible combinations are found, the best combination will be found through the Order class.
+   * Once the best combination is found, an update to the database will be made by removing the best combination's
+   * foods from the database using the updateInventory method in the Order class.
+   */
   public void buildHamper() throws OrderCannotBeValidatedException{
-    System.out.println("making hampers...");
     Inventory foodInventory = new Inventory();
+    foodMap = foodInventory.getFoodMap();
     for(int j = 1; j<1001; j++){
-      List<NutritionalProfile> generated = create(nutritionalNeedsFamily, j,foodInventory);
+      List<NutritionalProfile> generated = create(nutritionalNeedsFamily, j,foodMap);
       possible.add(generated);
     }
-    //System.out.println(numOfHampers);
     foodInventory.close();
     theOrder.bestCombo(hamperProfile);
     theOrder.updateInventory();
-    //return possible;
-    //List<List<NutritionalProfile>>
   }
   
-
-  public List<NutritionalProfile> create(Client familyNeeds, int j, Inventory foodInventory){
+/**
+   * @param familyNeeds - a Client object that holds the information about the weekly nutritional needs of a family
+   * @param j - the starting key used to access the foodInven HashMap
+   * @param foodInven - the HashMap holding the nutritional information about foods currently available in the database
+   * This method will create one possible combination for a hamper.
+   * First it will find a combination of foods that meet the grain requirements for the family.
+   * Using a for loop, it will access each nutritional profile in foodInven, starting at position j in the map.
+   * Then it will add the nutritional profile to the list called ourFood as long as the grain content is not 0,
+   * and each of the item's nutritional values will be added to the corresponding totalGrain, totalFV etc variables.
+   * If there does not exist an item associated with a key, the loop will move on and continue to the next item.
+   * If the totalGrains of the items exceeds the grain needs of the family, then it will break out of the loop.
+   * The second loop works similarly to the first loop which has been described above. Except it will only add items
+   * that have grain contents of 0 in order to not affect the totalGrains. A break will occur in this loop if
+   * the totalFV exceeds the FV needs of the family.
+   * The third loop also works similarly to the two loops described above. But it will only add items
+   * that have grain contents and FV contents of 0 in order to not affect the totalGrains and totalFV. 
+   * A break will occur in this loop if the totalPro exceeds the protein needs of the family.
+   * the totalFV exceeds the FV needs of the family.
+   * The fourth loop also works similarly to all the loops described above. But it will only add items
+   * that have grain contents, FV contents, and protein contents of 0 in order to not affect the totalGrains, totalFV,
+   * and totalPro values. A break will occur in this loop if the totalOther exceeds the other needs of the family.
+   * Finally, a new Client object will be created using the values of the totals of each category.
+   * Then this Client object will be added to the hamperProfile list to keep track of all the possible hamper combinations.
+   * @return List<NutritionalProfile> - returns a list of the nutritional profiles associated of the foods that were added to ourFood.
+   */
+  public List<NutritionalProfile> create(Client familyNeeds, int j, HashMap<Integer, NutritionalProfile> foodInven){
     List<NutritionalProfile> ourFood = new ArrayList<NutritionalProfile>();
     int totalGrain = 0;
     int totalFV = 0;
     int totalPro = 0;
     int totalOther = 0;
     int totalCal = 0;
-    HashMap<Integer, NutritionalProfile> foodInven = foodInventory.getFoodMap();
 
     for(int i = j; i<foodInven.size(); i++){
       if(foodInven.get(i) == null){
@@ -61,7 +93,6 @@ public class Hamper {
         break;
       }
     }
-    //System.out.println("Current grain:"+totalGrain);
 
     for(int i = j; i<foodInven.size(); i++){
       if(foodInven.get(i) == null){
@@ -78,7 +109,7 @@ public class Hamper {
         totalCal += foodInven.get(i).getCalories();
       }
     }
-    //System.out.println("Current FV:"+totalFV);
+
     for(int i = j; i<foodInven.size(); i++){
       if(foodInven.get(i) == null){
         continue;
@@ -95,7 +126,7 @@ public class Hamper {
         totalCal += foodInven.get(i).getCalories();
       }
     }
-    //System.out.println("Current Protien:"+totalPro);
+
     for(int i = j; i<foodInven.size(); i++){
       if(foodInven.get(i) == null){
         continue;
@@ -111,38 +142,19 @@ public class Hamper {
         totalCal += foodInven.get(i).getCalories();
       }
   }
-    //System.out.println("Current Other:"+totalOther);
+
     Client possibleProfile = new Client(totalGrain, totalFV, totalPro, totalOther, totalCal);
     hamperProfile.add(possibleProfile);
-    ourFood = trimRepeats(ourFood);
     return ourFood;
   } 
-  public List<NutritionalProfile> trimRepeats(List<NutritionalProfile> generated){
-    for(int i=1; i<generated.size(); i++){
-      int currID = generated.get(i).getID();
-      for(int j = 2; j<generated.size(); j++){
-        int theID = generated.get(j).getID();
-        if(theID == currID){
-          generated.remove(j);
-        }
-      }
-    }
-    return generated;
-  }
-  
 
   
   public Client getNutritionalNeedsFamily(){
     return nutritionalNeedsFamily;
   }
   
-
   public List<Client> getHamperProfile(){
       return hamperProfile;
-  }
-
-  public void setHamperProfile(List<Client> profile){
-    this.hamperProfile = profile;
   }
 
   public List<List<NutritionalProfile>> getPossible(){
@@ -151,6 +163,10 @@ public class Hamper {
 
   public void setPossible(List<List<NutritionalProfile>> givenPossible){
     this.possible = givenPossible;
+  }
+  
+  public void setHamperProfile(List<Client> profile){
+    this.hamperProfile = profile;
   }
 
   public Order getOrder(){
